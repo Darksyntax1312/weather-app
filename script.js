8737fdf7610496c6a81e9ea547e15fce9bf4346e
Nexorithm <mayankmadhukar67@gmail.com>
2026-08-20 17:22:16 +0530

    Add initial setup for weather app
    - Created HTML structure with card layout
    - Added CSS styling with glass-morphism effect
    - Set up JavaScript variables for DOM elements
    - Added placeholder text in input field

---
+++ script.js
@@ -0,0 +1,10 @@
+let Card1 = document.querySelector(".card");
+let container = document.querySelector(".container-1");
+let button1 = document.querySelector(".btn-2");
+let buttonDiv = document.querySelector(".btn-1");
+let inputBox = document.querySelector(".input-box");
+
+console.log("Weather App - Day 1");
+console.log("Card:", Card1);
+console.log("Input:", inputBox);
+console.log("Button:", button1);
